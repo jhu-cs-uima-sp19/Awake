dd74cce91b7038ae3a6c3ea0f74f31c084ce7c4a
Jixin Zheng <jixinzheng@Jixins-MacBook-Pro.local>
2019-12-05 04:48:02 -0500

    half fixed flashcardlistfragment

--- app/src/main/java/com/example/awake/FlashcardListFragment.java
+++ app/src/main/java/com/example/awake/FlashcardListFragment.java
@@ -1,5 +1,6 @@
 package com.example.awake;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import androidx.fragment.app.Fragment;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class FlashcardListFragment extends Fragment {
@@ -31,10 +33,34 @@ public class FlashcardListFragment extends Fragment {
         // ArrayList<Flashcard> list =
         // Use the view for this fragment to search for UI components.
         flashcard_list_view = (ListView) view.findViewById(R.id.flashcard_list);
-        FlashcardsAdapter adapter = new FlashcardsAdapter(list, this);
+        FlashcardsAdapter adapter = new FlashcardsAdapter(this, R.layout.flashcards_list_fragment, flashcards);
         flashcard_list_view.setAdapter(adapter);
 
         return view;
+    }
+
+    public void update_list_view() {
+        /*
+        I need to book keep that fact that the switch can turn by tapping and also by updating or setting
+        a new alarm.
+         */
+
+        Collections.sort(mA.alarms);
+
+        // make array adapter to bind arraylist to listview with new custom item layout
+        AlarmsAdapter aa = new AlarmsAdapter(mA, R.layout.alarm_entry, mA.alarms);
+        alarm_list_view.setAdapter(aa);
+        registerForContextMenu(alarm_list_view);
+        aa.notifyDataSetChanged();  // to refresh items in the list
+    }
+
+    // Used to access activity in fragment.
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof MainActivity){
+            mA =(MainActivity) context;
+        }
     }
 
 
